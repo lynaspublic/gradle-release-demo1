40a1aa6d14555dcd12812ac59704ee633d7b56d6
sazzad <sazzad@hsenidmobile.com>
2019-07-22 12:43:58 +0600

    init

---
+++ src/main/java/com/lynas/gradlereleasedemo1/GradleReleaseDemo1Application.java
@@ -0,0 +1,13 @@
+package com.lynas.gradlereleasedemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleReleaseDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleReleaseDemo1Application.class, args);
+    }
+
+}
